0eebbbff062ad7e88f23ee85d3256f8544f30709
Jason Wesson <jsnwesson@gmail.com>
2021-10-08 23:25:27 -0500

    solve twoSums with a hash table

--- twoSum/twoSum.js
+++ twoSum/twoSum.js
@@ -1,14 +1,27 @@
 //APPROACH 1: BRUTE FORCE
+// const twoSum = function(nums, target) {
+
+//   for (var i = 0; i < nums.length; i++) {
+//     for (var j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) {
+//         return [i, j]
+//       }
+//     }
+//   }
+
+// }
+
+//APPROACH 2: USING HASH TABLE
 const twoSum = function(nums, target) {
-
+  var hash = {};
   for (var i = 0; i < nums.length; i++) {
-    for (var j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j]
-      }
+    if (hash[target - nums[i]] !== undefined) {
+      var firstInd = hash[target - nums[i]];
+      var secondInd = i;
+      return [firstInd, secondInd];
     }
+    hash[nums[i]] = i;
   }
-
 }
 
 module.exports = twoSum;
